fb97349172f0d02d76c48c33f0989cd88d7d456b
ntnirathakur21 <ntnirajthakur21@gmail.com>
2022-10-19 23:45:02 +0545

    added try catch with async await while starting server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import app from "./app";
+require("dotenv").config();
 
 const server = app({
     logger: true,
     pluginTimeout: 20000,
 })
 
-server.listen({ port: 3000, host: 'localhost'}, (err, address)=>{
-    if(err){
-        server.log.error(err);
-        return process.exit(1);
+const start = async() => {
+    try {
+        const address = await server.listen({ port: parseInt(process.env.PORT!) || 3000, host: 'localhost'});
+        console.log(`Server listening at address : ${address}`);
     }
-    console.log(`Server started on port : 3000\n`);
-})
+    catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+}
+
+start();
