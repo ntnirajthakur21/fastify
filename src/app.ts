3f249f1769825e86e715f32f7255076056ab17be
ntnirathakur21 <ntnirajthakur21@gmail.com>
2022-10-26 16:54:37 +0545

    Added route for page not found

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import fastify, { FastifyServerOptions } from "fastify";
 const app = (opts: FastifyServerOptions = {}) => {
     const server = fastify(opts);
     server.get('/', async(req,res)=> 'Welcome to Fastify app');
+    server.get('*',async (req, res) => 'Page not found')
     return server;
 }
 
